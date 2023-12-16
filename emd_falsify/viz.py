0219cbaedffbbd6e1f309230dabf0d9d13f3b43e
Alexandre René <arene010@uottawa.ca>
2023-12-16 01:49:05 +0100

    [viz] Improvements to calibration plots

--- emd_falsify/viz.py
+++ emd_falsify/viz.py
@@ -6,7 +6,8 @@ import matplotlib as mpl
 import seaborn as sns
 import holoviews as hv
 
-from typing import NamedTuple, Optional
+from dataclasses import dataclass
+from typing import Optional, Dict, List
 
 from .config import config
 from . import utils
@@ -15,10 +16,22 @@ dash_patterns = ["dotted", "dashed", "solid"]
 
 ## Plotting functions ##
 
-class CalibrationPlotElements(NamedTuple):
+@dataclass
+class CalibrationPlotElements:
+    """
+    `bin_idcs`: Dictionary indicating which experiment index were assigned to
+       each bin. Use in conjunction with the EpistemicDistribution iterator
+       to reconstruct specific experiments.
+    """
     calibration_curves: hv.Overlay
     prohibited_areas   : hv.Area
     discouraged_areas : hv.Overlay
+    bin_idcs: Dict[float,List[np.ndarray[int]]]
+
+    def __iter__(self):
+        yield self.calibration_curves
+        yield self.prohibited_areas
+        yield self.discouraged_areas
 
     def _repr_mimebundle_(self, *args, **kwds):
         return (self.prohibited_areas * self.discouraged_areas * self.calibration_curves)._repr_mimebundle_(*args, **kwds)
@@ -28,7 +41,7 @@ def calibration_bins(calib_results: CalibrateResult,
     """Return the bin edges for the histograms produced by `calibration_plot`.
     
     .. Note:: These are generally *not* good bin edges for plotting a histogram
-    of calibration results: by design, they will produce an almost perfectly
+    of calibration results: by design, they will produce an almost
     flat histogram.
     """
     bin_edges = {}
@@ -66,26 +79,30 @@ def calibration_plot(calib_results: CalibrateResult,
 
     ## Calibration curves ##
     calib_curves = {}
+    calib_bins = {}
     for c, data in calib_results.items():
         # # We don’t do the following because it uses the Bconf data to break ties.
         # # If there are a lot equal values (typically happens with a too small c),
         # # then those will get sorted and we get an artificial jump from 0 to 1
         # data.sort(order="Bemd")
         σ = np.argsort(data["Bemd"])  # This will only use Bemd data; order within ties remains random
-        data["Bemd"] = data["Bemd"][σ]
-        data["Bconf"] = data["Bconf"][σ]
+        Bemd = data["Bemd"][σ]    # NB: Don’t modify original data order: 
+        Bconf = data["Bconf"][σ]  #     we may want to inspect it later.
 
         curve_data = []
+        bin_idcs = []
         i = 0
         for w in utils.get_bin_sizes(len(data), target_bin_size):
-            curve_data.append((data[i:i+w]["Bemd"].mean(),
-                               data[i:i+w]["Bconf"].mean()))
+            curve_data.append((Bemd[i:i+w].mean(),
+                               Bconf[i:i+w].mean()))
+            bin_idcs.append(σ[i:i+w])
             i += w
 
         curve = hv.Curve(curve_data, kdims="Bemd", vdims="Bconf", label=f"{c=}")
         curve = curve.redim.range(Bemd=(0,1), Bconf=(0,1))
         curve.opts(hv.opts.Curve(**config.viz.calibration_curves))
         calib_curves[c] = curve
+        calib_bins[c] = bin_idcs
 
     calib_hmap = hv.HoloMap(calib_curves, kdims=["c"])
 
@@ -113,7 +130,8 @@ def calibration_plot(calib_results: CalibrateResult,
 
     ## Combine & return ##
     return CalibrationPlotElements(
-        calib_hmap, prohibited_areas, discouraged_area_1*discouraged_area_2)
+        calib_hmap, prohibited_areas, discouraged_area_1*discouraged_area_2,
+        calib_bins)
 
 
 # sanitize = hv.core.util.sanitize_identifier_fn
